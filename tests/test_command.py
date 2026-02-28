fbfed6447e7a9513f974e495cf121ac50c240a5b
ELB <elboesen@gmail.com>
2026-02-28 11:20:27 +0100

    Made a command called 'app' that runs module.

--- tests/test_command.py
+++ tests/test_command.py
@@ -7,6 +7,5 @@ def test_running_module() -> None:
     assert not os.system('python -m app'), "Error was raised during module run"
 
 
-@pytest.mark.skip("Command is not implemented yet")
 def test_using_command() -> None:
     assert not os.system('app'), "Error was raised during 'app' command."
